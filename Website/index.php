f1aadd203b0dcaa7d4c57b766e7b162e19dfd18a
Bishwaroop Mukherjee <bishwaroop.mukherjee@gmail.com>
2017-09-05 16:41:54 +0530

    Removing unnecessary comments

--- Website/index.php
+++ Website/index.php
@@ -329,21 +329,9 @@ and open the template in the editor.
                             <a href="#home">Home</a>
                         </li>
                         <li class="footer-menu-divider">&sdot;</li>
-                        <!--
-                        <li>
-                            <a href="#about">About</a>
-                        </li>
-                        <li class="footer-menu-divider">&sdot;</li>
-                        -->
                         <li>
                             <a href="#services">Services</a>
                         </li>
-                        <!--
-                        <li class="footer-menu-divider">&sdot;</li>
-                        <li>
-                            <a href="#contact">Contact</a>
-                        </li>
-                        -->
                     </ul>
                     <p class="copyright text-muted small">Copyright &copy; Your Company 2014. All Rights Reserved</p>
                 </div>
